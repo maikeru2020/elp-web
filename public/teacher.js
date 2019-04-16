3a885c18be0559e0b3962ac0e8edf638ca1557f0
maikeru2020 <maikeru2020@gmail.com>
2019-04-15 21:57:17 -0700

    lesson content displays when you click left navigation

--- public/teacher.js
+++ public/teacher.js
@@ -1,8 +1,44 @@
 function getContent(number) {
     $.get("getLessonPlan", {week: number}, function(result) {
-        var content = "Week: " + result.week + "<br>" +
-                      "Due Date: " + result.due_date + "<br>" +
-                      "Topic: " + result.topic + "<br>";
-        $('#content').html(content);
+        var week = result.week;
+        var dueDate = formatDate(result.due_date);
+        var weekEnding = formatDate(result.week_ending);
+        var reference = result.reference;
+        var dayDuration = result.day_duration;
+        var topic = result.topic;
+        var objectives = result.objectives;
+        var activities = result.activities;
+        var materials = result.materials;
+        var corePoints = result.core_points;
+        var evaluation = result.evaluation;
+
+        var content = "<h1>Week " + week + "</h1>" +
+                      "Due Date:<br>" +
+                      "<input type='date' value='" + dueDate + "'><br>" +
+                      "Week Ending:<br>" +
+                      "<input type='date' value='" + weekEnding + "'><br>" +
+                      "Reference:<br>" +
+                      "<textarea>" + reference + "</textarea><br>" +
+                      "Day / Duration:<br>" +
+                      "<textarea>" + dayDuration + "</textarea><br>" +
+                      "Topic:<br>" +
+                      "<textarea>" + topic + "</textarea><br>" +
+                      "Objectives / R.P.K.:<br>" +
+                      "<textarea>" + objectives + "</textarea><br>" +
+                      "Teacher-Learner Activities:<br>" +
+                      "<textarea>" + activities + "</textarea><br>" +
+                      "Teaching Learning Materials:<br>" +
+                      "<textarea>" + materials + "</textarea><br>" +
+                      "Core Points:<br>" +
+                      "<textarea>" + corePoints + "</textarea><br>" +
+                      "Evaluation and Remarks:<br>" +
+                      "<textarea>" + evaluation + "</textarea><br>" +
+                      "<button>Save</button>";
+
+        $('.content').html(content);
     });
+}
+
+function formatDate(date) {
+    return date.split('T')[0];
 }
